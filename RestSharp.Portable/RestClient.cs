9458dab3badc46edf3ef307ec13e1c1715cb933f
Mark Junker <mark.junker@dataline.de>
2013-11-13 09:27:48 +0100

    Grant access to POST data with an extension method

--- RestSharp.Portable/RestClient.cs
+++ RestSharp.Portable/RestClient.cs
@@ -87,42 +87,6 @@ namespace RestSharp.Portable
             return message;
         }
 
-        private HttpContent GetBodyData(IRestRequest request)
-        {
-            HttpContent content;
-            var body = request.Parameters.FirstOrDefault(x => x.Type == ParameterType.RequestBody);
-            if (body != null)
-            {
-                MediaTypeHeaderValue contentType;
-                byte[] buffer;
-                if (body.Value is byte[])
-                {
-                    buffer = (byte[])body.Value;
-                    contentType = body.ContentType;
-                }
-                else
-                {
-                    buffer = request.Serializer.Serialize(body.Value);
-                    contentType = request.Serializer.ContentType;
-                }
-                content = new ByteArrayContent(buffer);
-                content.Headers.ContentType = contentType;
-                content.Headers.ContentLength = buffer.Length;
-            }
-            else if (request.Method == HttpMethod.Post)
-            {
-                var postData = request.Parameters.Where(x => x.Type == ParameterType.GetOrPost)
-                    .Select(x => new KeyValuePair<string,string>(x.Name, string.Format("{0}", x.Value)))
-                    .ToList();
-                content = new FormUrlEncodedContent(postData);
-            }
-            else
-            {
-                content = null;
-            }
-            return content;
-        }
-
         private HttpClient CreateHttpClient(IRestRequest request)
         {
             HttpClient httpClient;
@@ -148,7 +112,7 @@ namespace RestSharp.Portable
             var httpClient = CreateHttpClient(request);
             var message = CreateHttpRequestMessage(request);
             
-            var bodyData = GetBodyData(request);
+            var bodyData = request.GetContent();
             if (bodyData != null)
                 message.Content = bodyData;
 
---
+++ RestSharp.Portable/RestRequestExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace RestSharp.Portable
+{
+    public static class RestRequestExtensions
+    {
+        public static HttpContent GetContent(this IRestRequest request)
+        {
+            HttpContent content;
+            var body = request.Parameters.FirstOrDefault(x => x.Type == ParameterType.RequestBody);
+            if (body != null)
+            {
+                MediaTypeHeaderValue contentType;
+                byte[] buffer;
+                if (body.Value is byte[])
+                {
+                    buffer = (byte[])body.Value;
+                    contentType = body.ContentType;
+                }
+                else
+                {
+                    buffer = request.Serializer.Serialize(body.Value);
+                    contentType = request.Serializer.ContentType;
+                }
+                content = new ByteArrayContent(buffer);
+                content.Headers.ContentType = contentType;
+                content.Headers.ContentLength = buffer.Length;
+            }
+            else if (request.Method == HttpMethod.Post)
+            {
+                var postData = request.Parameters.Where(x => x.Type == ParameterType.GetOrPost)
+                    .Select(x => new KeyValuePair<string, string>(x.Name, string.Format("{0}", x.Value)))
+                    .ToList();
+                content = new FormUrlEncodedContent(postData);
+            }
+            else
+            {
+                content = null;
+            }
+            return content;
+        }
+    }
+}
