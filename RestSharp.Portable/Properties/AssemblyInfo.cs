36a012ea7bf8bc8a037ea894925067d7f6262f32
Mark Junker <mark.junker@dataline.de>
2013-11-18 20:01:53 +0100

    Bumped version to 1.0.6

--- RestSharp.Portable/Properties/AssemblyInfo.cs
+++ RestSharp.Portable/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // durch Einsatz von '*', wie in nachfolgendem Beispiel:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
