78f42897f7950559e5f34c65888fcf0a726e2dee
Mark Junker <mark.junker@dataline.de>
2013-11-26 10:25:26 +0100

    Test for content type deserializer replacement

---
+++ RestSharp.Portable.Test/Deserializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RestSharp.Portable.Test
+{
+    [TestClass]
+    public class Deserializer
+    {
+        class CustomDeserializer : RestSharp.Portable.Deserializers.JsonDeserializer
+        {
+            protected override void ConfigureSerializer(Newtonsoft.Json.JsonSerializer serializer)
+            {
+                base.ConfigureSerializer(serializer);
+                serializer.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.MicrosoftDateFormat;
+            }
+        }
+
+        [TestMethod]
+        public void TestReplaceContentTypeDeserializer()
+        {
+            var restClient = new RestClient();
+            var deserializer = new CustomDeserializer();
+            restClient.ReplaceHandler(typeof(RestSharp.Portable.Deserializers.IDeserializer), deserializer);
+            Assert.AreSame(deserializer, restClient.GetHandler("application/json"));
+            Assert.AreSame(deserializer, restClient.GetHandler("text/json"));
+        }
+    }
+}
