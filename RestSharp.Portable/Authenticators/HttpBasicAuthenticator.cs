8459095ee329d9d34a41e1f30b75741e209ad822
Mark Junker <mark.junker@dataline.de>
2013-11-26 10:02:58 +0100

    Added some standard authenticators

---
+++ RestSharp.Portable/Authenticators/HttpBasicAuthenticator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RestSharp.Portable.Authenticators
+{
+    /// <summary>
+    /// The default HTTP Basic authenticator
+    /// </summary>
+    public class HttpBasicAuthenticator : IAuthenticator
+    {
+        private readonly string _username;
+        private readonly string _password;
+
+        /// <summary>
+        /// Constructor taking the user name and password for the HTTP Basic authenticator
+        /// </summary>
+        /// <param name="username">User name</param>
+        /// <param name="password">Password</param>
+        public HttpBasicAuthenticator(string username, string password)
+        {
+            _password = password;
+            _username = username;
+        }
+
+        /// <summary>
+        /// Modifies the request to ensure that the authentication requirements are met.
+        /// </summary>
+        /// <param name="client">Client executing this request</param>
+        /// <param name="request">Request to authenticate</param>
+        public void Authenticate(IRestClient client, IRestRequest request)
+        {
+            // only add the Authorization parameter if it hasn't been added by a previous Execute
+            if (request.Parameters.Any(p => p.Name.Equals("Authorization", StringComparison.OrdinalIgnoreCase)))
+                return;
+            var token = Convert.ToBase64String(Encoding.UTF8.GetBytes(string.Format("{0}:{1}", _username, _password)));
+            var authHeader = string.Format("Basic {0}", token);
+            request.AddParameter("Authorization", authHeader, ParameterType.HttpHeader);
+        }
+    }
+}
