5b49d14bc3f97a842a03acfe05a87f2d0ee8db48
Mark Junker <mark.junker@dataline.de>
2014-01-22 18:11:37 +0100

    Support for C++/CLI

---
+++ NuGet/RestSharp.Portable.Encodings/install.ps1
@@ -0,0 +1,7 @@
+param($installPath, $toolsPath, $package, $project)
+
+$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\lib\portable-net4+sl5+wp8+win8\RestSharp.Portable.Encodings.dll")
+$obj = $project.Object
+$getRefsMethod = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_References")
+$refs = $getRefsMethod.Invoke($obj, $null)
+$refs.Add($assemblyPath)
