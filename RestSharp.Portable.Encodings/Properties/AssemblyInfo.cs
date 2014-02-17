d4366a0e3997a1cfcba7091d25007d29a6e568c0
Mark Junker <mark.junker@dataline.de>
2014-02-17 14:28:40 +0100

    Bumped versions

--- RestSharp.Portable.Encodings/Properties/AssemblyInfo.cs
+++ RestSharp.Portable.Encodings/Properties/AssemblyInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // durch Einsatz von '*', wie in nachfolgendem Beispiel:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1.0")]
-[assembly: AssemblyFileVersion("1.4.2.2")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
