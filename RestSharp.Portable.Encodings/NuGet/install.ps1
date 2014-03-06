95eded4cb0d5436d16c06f3d7df2430a6155eca7
Mark Junker <mark.junker@dataline.de>
2014-03-06 16:02:37 +0100

    Fixed C++/CLI targets

--- RestSharp.Portable.Encodings/NuGet/install.ps1
+++ RestSharp.Portable.Encodings/NuGet/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net45+wp8+win8\RestSharp.Portable.Encodings.dll")
+$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net45+wp80+win\RestSharp.Portable.Encodings.dll")
 $obj = $project.Object
 $getRefsMethod = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_References")
 $refs = $getRefsMethod.Invoke($obj, $null)
--- RestSharp.Portable/NuGet/install.ps1
+++ RestSharp.Portable/NuGet/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net4+sl5+wp8+win8\RestSharp.Portable.dll")
+$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net40+sl50+wp80+win\RestSharp.Portable.dll")
 $obj = $project.Object
 $getRefsMethod = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_References")
 $refs = $getRefsMethod.Invoke($obj, $null)
