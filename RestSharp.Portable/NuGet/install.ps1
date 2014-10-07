c7f4f11fbabf30f55b3344369f4747392eedf55d
Mark Junker <mark.junker@dataline.de>
2014-10-07 14:01:59 +0200

    C++/CLI fixes

--- RestSharp.Portable/NuGet/install.ps1
+++ RestSharp.Portable/NuGet/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net40+sl50+wp80+win\RestSharp.Portable.dll")
+$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net40+sl50+win+wp80+MonoAndroid10+MonoTouch10\RestSharp.Portable.dll")
 $obj = $project.Object
 $getRefsMethod = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_References")
 $refs = $getRefsMethod.Invoke($obj, $null)
