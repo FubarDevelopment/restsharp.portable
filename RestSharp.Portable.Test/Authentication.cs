7acc866397de96e093d9ecab174ffa15a2124aeb
Mark Junker <mark.junker@dataline.de>
2014-10-28 20:10:44 +0100

    Test case for HTTP digest authentication

---
+++ RestSharp.Portable.Test/Authentication.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace RestSharp.Portable.Test
+{
+    [TestClass]
+    public class Authentication
+    {
+        class AuthenticationResult
+        {
+            public bool Authenticated { get; set; }
+            public string User { get; set; }
+        }
+
+        [TestMethod]
+        public async Task TestHttpDigest()
+        {
+            const string username = "user";
+            const string password = "passwd";
+
+            var client = new RestClient("http://httpbin.org");
+            client.CookieContainer = new System.Net.CookieContainer();
+            client.Authenticator = new Authenticators.HttpDigestAuthenticator(new System.Net.NetworkCredential(username, password));
+            var request = new RestRequest("digest-auth/auth/{username}/{password}", System.Net.Http.HttpMethod.Get);
+            request.AddUrlSegment("username", username);
+            request.AddUrlSegment("password", password);
+            var response = await client.Execute<AuthenticationResult>(request);
+
+            Assert.AreEqual(true, response.Data.Authenticated);
+            Assert.AreEqual(username, response.Data.User);
+        }
+    }
+}
