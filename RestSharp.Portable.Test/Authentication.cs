2c5962ada1facca0e2faaa02b8e42cc5d179c74a
Mark Junker <mark.junker@dataline.de>
2014-10-29 16:33:22 +0100

    Added test for auth-int

--- RestSharp.Portable.Test/Authentication.cs
+++ RestSharp.Portable.Test/Authentication.cs
@@ -14,7 +14,7 @@ namespace RestSharp.Portable.Test
         }
 
         [TestMethod]
-        public async Task TestHttpDigest()
+        public async Task TestHttpDigestAuth()
         {
             const string username = "user";
             const string password = "passwd";
@@ -23,6 +23,24 @@ namespace RestSharp.Portable.Test
             client.CookieContainer = new System.Net.CookieContainer();
             client.Authenticator = new Authenticators.HttpDigestAuthenticator(new System.Net.NetworkCredential(username, password));
             var request = new RestRequest("digest-auth/auth/{username}/{password}", System.Net.Http.HttpMethod.Get);
+            request.AddUrlSegment("username", username);
+            request.AddUrlSegment("password", password);
+            var response = await client.Execute<AuthenticationResult>(request);
+
+            Assert.AreEqual(true, response.Data.Authenticated);
+            Assert.AreEqual(username, response.Data.User);
+        }
+
+        [TestMethod]
+        public async Task TestHttpDigestAuthInt()
+        {
+            const string username = "user";
+            const string password = "passwd";
+
+            var client = new RestClient("http://httpbin.org");
+            client.CookieContainer = new System.Net.CookieContainer();
+            client.Authenticator = new Authenticators.HttpDigestAuthenticator(new System.Net.NetworkCredential(username, password));
+            var request = new RestRequest("digest-auth/auth-int/{username}/{password}", System.Net.Http.HttpMethod.Get);
             request.AddUrlSegment("username", username);
             request.AddUrlSegment("password", password);
             var response = await client.Execute<AuthenticationResult>(request);
