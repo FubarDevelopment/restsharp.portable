04f608ee96de2c1558133b723bb7f3c6e58214db
Mark Junker <mark.junker75@gmail.com>
2014-11-04 22:43:17 +0100

    Modification for C++/CLI

--- RestSharp.Portable.Encodings/NuGet/install.ps1
+++ RestSharp.Portable.Encodings/NuGet/install.ps1
@@ -1,6 +1,6 @@
 param($installPath, $toolsPath, $package, $project)
 
-$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net45+win+wp80+MonoAndroid10+MonoTouch10\RestSharp.Portable.Encodings.dll")
+$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net45+win+wpa81+wp80+MonoAndroid10+MonoTouch10\RestSharp.Portable.Encodings.dll")
 $obj = $project.Object
 $getRefsMethod = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_References")
 $refs = $getRefsMethod.Invoke($obj, $null)
