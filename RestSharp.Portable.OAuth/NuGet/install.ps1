52a75db52f62704adb5f56a50a0d9e7c03d198dc
Mark Junker <m.junker@fubar-dev.de>
2014-11-07 11:07:54 +0100

    NuGet package for the OAuth 1.0 authenticator

---
+++ RestSharp.Portable.OAuth/NuGet/install.ps1
@@ -0,0 +1,7 @@
+param($installPath, $toolsPath, $package, $project)
+
+$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net40+sl50+win+wpa81+wp80+MonoAndroid10+MonoTouch10\RestSharp.Portable.OAuth.dll")
+$obj = $project.Object
+$getRefsMethod = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_References")
+$refs = $getRefsMethod.Invoke($obj, $null)
+$refs.Add($assemblyPath)
