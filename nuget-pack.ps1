a2fe5d46eedac9c0ce7d168f6e38f29bb41b4d14
Mark Junker <m.junker@fubar-dev.de>
2014-11-07 12:20:15 +0100

    Pack OAuth too (with an optional -config parameter)

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,7 +1,11 @@
+[OutputType([void])]
 param(
 	[Parameter(Mandatory = $true)]
-	$version
+	$version,
+	[Parameter()]
+	$config = "Release"
 )
 
-& nuget pack .\RestSharp.Portable\RestSharp.Portable.csproj -Properties Configuration=Release -Version $version
-& nuget pack .\RestSharp.Portable.Encodings\RestSharp.Portable.Encodings.csproj -Properties Configuration=Release -Version $version
+& nuget pack .\RestSharp.Portable\RestSharp.Portable.csproj -Properties Configuration=$config -Version $version
+& nuget pack .\RestSharp.Portable.Encodings\RestSharp.Portable.Encodings.csproj -Properties Configuration=$config -Version $version
+& nuget pack .\RestSharp.Portable.OAuth\RestSharp.Portable.OAuth.csproj -Properties Configuration=$config -Version $version
