8699c45d3ef80ee9b9f5bba23d8916f841d6656d
Mark Junker <m.junker@fubar-dev.de>
2014-11-11 11:17:06 +0100

    Bumped version

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,11 +1,11 @@
 [OutputType([void])]
 param(
 	[Parameter(Mandatory = $true)]
-	$version = "1.8.2",
+	$version = "1.8.3",
 	[Parameter()]
 	$config = "Release",
 	[Parameter()]
-	$oauth2version = "0.8.34.2"
+	$oauth2version = "0.8.34.3"
 )
 
 & nuget pack .\RestSharp.Portable\RestSharp.Portable.csproj -Properties Configuration=$config -Version $version
