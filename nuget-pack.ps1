687dc0222cdcf79013499ad3b4fc87a62b788704
Mark Junker <m.junker@fubar-dev.de>
2014-11-11 11:05:52 +0100

    Workaround for NuGet pack bug

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -10,5 +10,5 @@ param(
 
 & nuget pack .\RestSharp.Portable\RestSharp.Portable.csproj -Properties Configuration=$config -Version $version
 & nuget pack .\RestSharp.Portable.Encodings\RestSharp.Portable.Encodings.csproj -Properties Configuration=$config -Version $version
-& nuget pack .\RestSharp.Portable.OAuth\RestSharp.Portable.OAuth.csproj -Properties Configuration=$config -Version $version
+& nuget pack .\RestSharp.Portable.OAuth\RestSharp.Portable.OAuth.csproj -Properties "Configuration=$config;clientversion=$version" -Version $version
 & nuget pack .\RestSharp.Portable.OAuth2\RestSharp.Portable.OAuth2.csproj -Properties "Configuration=$config;clientversion=$version" -Version $oauth2version
