20c1b496164345e7ad060d998febd8b4098c0f57
Mark Junker <m.junker@fubar-dev.de>
2014-11-13 11:58:18 +0100

    Prepare for move to GitHub

--- RestSharp.Portable.OAuth2.Configuration/Properties/AssemblyInfo.cs
+++ RestSharp.Portable.OAuth2.Configuration/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8")]
-[assembly: AssemblyFileVersion("0.8")]
+[assembly: AssemblyVersion("0.8.34.4")]
+[assembly: AssemblyFileVersion("0.8.34.4")]
