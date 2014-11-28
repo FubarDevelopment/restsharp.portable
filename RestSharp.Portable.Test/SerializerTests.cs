d2b179400317e57c03a4e056ab39b1152abf4ac8
Mark Junker <m.junker@fubar-dev.de>
2014-11-28 15:58:05 +0100

    Added test for issure #3

---
+++ RestSharp.Portable.Test/SerializerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.Serialization;
+using System.Linq;
+
+namespace RestSharp.Portable.Test
+{
+    [TestClass]
+    public class SerializerTests
+    {
+        [DataContract(Namespace = "")]
+        class TestData
+        {
+            [DataMember]
+            public string TestProp { get; set; }
+        }
+
+        [TestMethod]
+        public void TestXmlSerializerBom()
+        {
+            var serializer = new RestSharp.Portable.Serializers.XmlDataContractSerializer();
+            var input = new TestData { TestProp = "propValue1" };
+            var output = serializer.Serialize(input);
+            var lastThreeBytes = new byte[3];
+            Array.Copy(output, output.Length - 3, lastThreeBytes, 0, 3);
+            CollectionAssert.AreNotEqual(new byte[] { 0xEF, 0xBB, 0xBF }, lastThreeBytes);
+        }
+    }
+}
