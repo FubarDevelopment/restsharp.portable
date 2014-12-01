0aeabb0f872c0f2e4fbc6e94ff0cf9ec2cab95a5
Mark Junker <m.junker@fubar-dev.de>
2014-12-01 11:58:16 +0100

    Moved ParameterType to a separate file

--- RestSharp.Portable/Parameter.cs
+++ RestSharp.Portable/Parameter.cs
@@ -8,37 +8,6 @@ using System.Threading.Tasks;
 namespace RestSharp.Portable
 {
     /// <summary>
-    /// Parameter type
-    /// </summary>
-    public enum ParameterType
-    {
-        /// <summary>
-        /// Parameter will be stored in the URL query for a GET request, or in the body for a POST request
-        /// </summary>
-        GetOrPost,
-        /// <summary>
-        /// The parameter is part of the IRestResponse.Resource
-        /// </summary>
-        UrlSegment,
-        /// <summary>
-        /// The parameter is part of the resulting URL query
-        /// </summary>
-        QueryString,
-        /// <summary>
-        /// The parameter will be sent as HTTP header
-        /// </summary>
-        HttpHeader,
-        /// <summary>
-        /// The parameter will be sent in the HTTP POST body
-        /// </summary>
-        RequestBody,
-        /// <summary>
-        /// The parameter will be sent as cookie value.
-        /// </summary>
-        Cookie,
-    }
-
-    /// <summary>
     /// Parameter container for REST requests
     /// </summary>
     public class Parameter
