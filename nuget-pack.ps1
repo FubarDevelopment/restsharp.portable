79ed029a05576529e547cfa81e31db4009660ec6
Mark Junker <mark.junker75@gmail.com>
2014-12-03 22:07:15 +0100

    Script update with new versions

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -1,11 +1,11 @@
 [OutputType([void])]
 param(
 	[Parameter()]
-	$version = "1.9.0",
+	$version = "1.9.1",
 	[Parameter()]
 	$config = "Release",
 	[Parameter()]
-	$oauth2version = "0.8.34.6"
+	$oauth2version = "0.8.34.7"
 )
 
 & nuget pack .\RestSharp.Portable\RestSharp.Portable.csproj -Properties Configuration=$config -Version $version
