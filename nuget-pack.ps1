70fd934624b3798eba04637b876bfd23b57441eb
Mark Junker <m.junker@fubar-dev.de>
2014-12-15 17:08:32 +0100

    We now use the same version for the OAuth2 package

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -3,12 +3,10 @@ param(
 	[Parameter()]
 	$version = "1.9.2",
 	[Parameter()]
-	$config = "Release",
-	[Parameter()]
-	$oauth2version = "0.8.34.8"
+	$config = "Release"
 )
 
 & nuget pack .\RestSharp.Portable.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
 & nuget pack .\RestSharp.Portable.Encodings.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
-& nuget pack .\RestSharp.Portable.OAuth\RestSharp.Portable.OAuth.csproj -Properties "Configuration=$config;clientversion=$version" -Version $version
-& nuget pack .\RestSharp.Portable.OAuth2\RestSharp.Portable.OAuth2.csproj -Properties "Configuration=$config;clientversion=$version" -Version $oauth2version
+& nuget pack .\RestSharp.Portable.OAuth.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
+& nuget pack .\RestSharp.Portable.OAuth2.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
