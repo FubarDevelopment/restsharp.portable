043976901aee06cfa3f11b10bb3a32a274db6b34
Mark Junker <m.junker@fubar-dev.de>
2014-12-16 08:42:05 +0100

    - Added a .NET 4.0 specific version for RestSharp.Portable.OAuth - Modified the nuspec file to reflect the latest changes

--- NuGet/Scripts/RestSharp.Portable.OAuth2/install.ps1
+++ NuGet/Scripts/RestSharp.Portable.OAuth2/install.ps1
@@ -1,6 +1,15 @@
 param($installPath, $toolsPath, $package, $project)
 
-$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\portable-net403+sl50+win+wpa81+wp80+MonoAndroid10+MonoTouch10\RestSharp.Portable.OAuth2.dll")
+$getTargetFrameworkMoniker = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_TargetFrameworkMoniker")
+$targetFrameworkVersion = (New-Object System.Runtime.Versioning.FrameworkName $getTargetFrameworkMoniker.Invoke($project.Object, $null)).Version
+$ver45 = New-Object System.Version 4,5
+
+$fwPath = "net40"
+if ($targetFrameworkVersion -ge $ver45) {
+	$fwPath = "net45"
+}
+
+$assemblyPath = [System.IO.Path]::Combine($toolsPath, "..\..\lib\$fwPath\RestSharp.Portable.OAuth2.dll")
 $obj = $project.Object
 $getRefsMethod = [Microsoft.VisualStudio.Project.VisualC.VCProjectEngine.VCProjectShim].GetMethod("get_References")
 $refs = $getRefsMethod.Invoke($obj, $null)
