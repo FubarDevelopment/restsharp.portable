d8b647af837d0745abd56f2c7c00f624f30e26f8
Mark Junker <m.junker@fubar-dev.de>
2015-01-14 12:17:13 +0100

    - Refactoring - Uses native URL encoding where possible - Moved escape functions from UrlUtility to UrlEscapeUtility to make it more testable

---
+++ RestSharp.Portable.Test/UrlEncodeVariant2Tests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Web;
+
+namespace RestSharp.Portable.Test
+{
+    [TestClass]
+    public class UrlEncodeVariant2Tests
+    {
+        private readonly UrlEscapeUtility s_utility = new UrlEscapeUtility(false);
+
+        [TestMethod]
+        public void TestEscapeDataStringComplianceASCII()
+        {
+            var chars = new string(Enumerable.Range(32, 95).Select(x => (char)x).ToArray());
+            var expected = Uri.EscapeDataString(chars);
+            var test = s_utility.Escape(chars, UrlEscapeFlags.BuilderVariantListByteArray);
+            Assert.AreEqual(expected, test);
+        }
+
+        [TestMethod]
+        public void TestEscapeDataStringComplianceUmlaut()
+        {
+            var chars = "äöüßÄÖÜ\u007F";
+            var expected = Uri.EscapeDataString(chars);
+            var test = s_utility.Escape(chars, UrlEscapeFlags.BuilderVariantListByteArray);
+            Assert.AreEqual(expected, test);
+        }
+
+        [TestMethod]
+        public void TestUrlEncodeComplianceASCII()
+        {
+            var chars = new string(Enumerable.Range(32, 95).Select(x => (char)x).ToArray());
+            var expected = HttpUtility.UrlEncode(chars);
+            var test = s_utility.Escape(chars, UrlEscapeFlags.LikeUrlEncode | UrlEscapeFlags.BuilderVariantListByteArray);
+            Assert.AreEqual(expected, test);
+        }
+
+        [TestMethod]
+        public void TestUrlEncodeComplianceUmlaut()
+        {
+            var chars = "äöüßÄÖÜ\u007F";
+            var expected = HttpUtility.UrlEncode(chars);
+            var test = s_utility.Escape(chars, UrlEscapeFlags.LikeUrlEncode | UrlEscapeFlags.BuilderVariantListByteArray);
+            Assert.AreEqual(expected, test);
+        }
+    }
+}
--- RestSharp.Portable/UrlUtility.cs
+++ RestSharp.Portable/UrlUtility.cs
@@ -10,43 +10,20 @@ namespace RestSharp.Portable
     /// </summary>
     internal static class UrlUtility
     {
-        private static readonly Encoding s_defaultEncoding = new UTF8Encoding(false);
-
-        private static readonly byte[] s_reserved = Encoding.UTF8.GetBytes(";/?:@&=+$,");
+        //private static readonly byte[] s_reserved = Encoding.UTF8.GetBytes(";/?:@&=+$,");
         private static readonly byte[] s_lowalpha = Encoding.UTF8.GetBytes("abcdefghijklmnopqrstuvwxyz");
         private static readonly byte[] s_upalpha = Encoding.UTF8.GetBytes("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
         private static readonly byte[] s_alpha = s_lowalpha.Union(s_upalpha).ToArray();
         private static readonly byte[] s_digit = Encoding.UTF8.GetBytes("0123456789");
-        private static readonly byte[] s_alphanum = s_alpha.Union(s_digit).ToArray();
+        internal static readonly byte[] s_alphanum = s_alpha.Union(s_digit).ToArray();
         private static readonly byte[] s_mark = Encoding.UTF8.GetBytes("-_.!~*'()");
-        private static readonly byte[] s_unreserved = s_alphanum.Union(s_mark).ToArray();
-        private static readonly byte[] s_control = Encoding.UTF8.GetBytes("\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008\u0009\u000a\u000b\u000c\u000d\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f");
-        private static readonly byte[] s_space = Encoding.UTF8.GetBytes(" ");
-        private static readonly byte[] s_delims = Encoding.UTF8.GetBytes("<>#%\"");
-        private static readonly byte[] s_unwise = Encoding.UTF8.GetBytes("{}|\\^[]`");
+        internal static readonly byte[] s_unreserved = s_alphanum.Union(s_mark).ToArray();
+        //private static readonly byte[] s_control = Encoding.UTF8.GetBytes("\u0000\u0001\u0002\u0003\u0004\u0005\u0006\u0007\u0008\u0009\u000a\u000b\u000c\u000d\u000e\u000f\u0010\u0011\u0012\u0013\u0014\u0015\u0016\u0017\u0018\u0019\u001a\u001b\u001c\u001d\u001e\u001f");
+        //private static readonly byte[] s_space = Encoding.UTF8.GetBytes(" ");
+        //private static readonly byte[] s_delims = Encoding.UTF8.GetBytes("<>#%\"");
+        //private static readonly byte[] s_unwise = Encoding.UTF8.GetBytes("{}|\\^[]`");
 
-        private static readonly byte[] s_hexCharsLow = Encoding.UTF8.GetBytes("0123456789abcdef");
-        private static readonly byte[] s_hexCharsUp = Encoding.UTF8.GetBytes("0123456789ABCDEF");
-
-        private delegate byte[] EscapeBuilderDelegate(byte[] data, UrlEscapeFlags flags, byte[] hexChars, ISet<byte> allowedBytes);
-
-        private static readonly Dictionary<UrlEscapeFlags, ISet<byte>> s_allowedBytes = new Dictionary<UrlEscapeFlags, ISet<byte>>
-        {
-            { UrlEscapeFlags.AllowLikeEscapeDataString, new HashSet<byte>(s_alphanum.Union(Encoding.UTF8.GetBytes("-_.~"))) },
-            { UrlEscapeFlags.AllowAllUnreserved, new HashSet<byte>(s_unreserved) },
-            { UrlEscapeFlags.AllowLikeUrlEncode, new HashSet<byte>(s_alphanum.Union(Encoding.UTF8.GetBytes("-_.!*()"))) },
-        };
-
-        private static readonly Dictionary<UrlEscapeFlags, EscapeBuilderDelegate> s_escapeBuilders = new Dictionary<UrlEscapeFlags, EscapeBuilderDelegate>
-        {
-            { UrlEscapeFlags.BuilderVariantListByte, EscapeToBytes1 },
-            { UrlEscapeFlags.BuilderVariantListByteArray, EscapeToBytes2 },
-        };
-
-        private static string ConvertEscapedBytesToString(byte[] data)
-        {
-            return s_defaultEncoding.GetString(data, 0, data.Length);
-        }
+        private static readonly UrlEscapeUtility s_defaultEscapeUtility = new UrlEscapeUtility();
 
         /// <summary>
         /// URL escape
@@ -55,7 +32,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static string Escape(string data)
         {
-            return Escape(data, UrlEscapeFlags.Default);
+            return s_defaultEscapeUtility.Escape(data);
         }
 
         /// <summary>
@@ -66,7 +43,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static string Escape(string data, Encoding encoding)
         {
-            return Escape(data, encoding, UrlEscapeFlags.Default);
+            return s_defaultEscapeUtility.Escape(data, encoding);
         }
 
         /// <summary>
@@ -76,7 +53,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static string Escape(byte[] data)
         {
-            return Escape(data, UrlEscapeFlags.Default);
+            return s_defaultEscapeUtility.Escape(data);
         }
 
         /// <summary>
@@ -86,7 +63,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static byte[] EscapeToBytes(string data)
         {
-            return EscapeToBytes(data, UrlEscapeFlags.Default);
+            return s_defaultEscapeUtility.EscapeToBytes(data);
         }
 
         /// <summary>
@@ -97,7 +74,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static byte[] EscapeToBytes(string data, Encoding encoding)
         {
-            return EscapeToBytes(data, encoding, UrlEscapeFlags.Default);
+            return s_defaultEscapeUtility.EscapeToBytes(data, encoding);
         }
 
         /// <summary>
@@ -107,7 +84,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static byte[] EscapeToBytes(byte[] data)
         {
-            return EscapeToBytes(data, UrlEscapeFlags.Default);
+            return s_defaultEscapeUtility.EscapeToBytes(data);
         }
 
         /// <summary>
@@ -118,7 +95,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static string Escape(string data, UrlEscapeFlags flags)
         {
-            return ConvertEscapedBytesToString(EscapeToBytes(data, flags));
+            return s_defaultEscapeUtility.Escape(data, flags);
         }
 
         /// <summary>
@@ -130,7 +107,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static string Escape(string data, Encoding encoding, UrlEscapeFlags flags)
         {
-            return ConvertEscapedBytesToString(EscapeToBytes(data, encoding, flags));
+            return s_defaultEscapeUtility.Escape(data, encoding, flags);
         }
 
         /// <summary>
@@ -141,7 +118,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static string Escape(byte[] data, UrlEscapeFlags flags)
         {
-            return ConvertEscapedBytesToString(EscapeToBytes(data, flags));
+            return s_defaultEscapeUtility.Escape(data, flags);
         }
 
         /// <summary>
@@ -152,7 +129,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static byte[] EscapeToBytes(string data, UrlEscapeFlags flags)
         {
-            return EscapeToBytes(data, s_defaultEncoding, flags);
+            return s_defaultEscapeUtility.EscapeToBytes(data, flags);
         }
 
         /// <summary>
@@ -164,7 +141,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static byte[] EscapeToBytes(string data, Encoding encoding, UrlEscapeFlags flags)
         {
-            return EscapeToBytes(encoding.GetBytes(data), flags);
+            return s_defaultEscapeUtility.EscapeToBytes(data, encoding, flags);
         }
 
         /// <summary>
@@ -175,169 +152,7 @@ namespace RestSharp.Portable
         /// <returns></returns>
         public static byte[] EscapeToBytes(byte[] data, UrlEscapeFlags flags)
         {
-            var hexChars = ((flags & UrlEscapeFlags.LowerCaseHexCharacters) != UrlEscapeFlags.Default) ? s_hexCharsLow : s_hexCharsUp;
-
-            var allowedBytesIndex = (flags & UrlEscapeFlags.AllowMask);
-            ISet<byte> allowedBytes;
-            if (!s_allowedBytes.TryGetValue(allowedBytesIndex, out allowedBytes))
-                allowedBytes = s_allowedBytes[UrlEscapeFlags.Default];
-
-            var builderVariant = (flags & UrlEscapeFlags.BuilderVariantMask);
-            EscapeBuilderDelegate builder;
-            if (!s_escapeBuilders.TryGetValue(builderVariant, out builder))
-                builder = EscapeToBytes2;
-            return builder(data, flags, hexChars, allowedBytes);
-        }
-
-        /// <summary>
-        /// Variant 1 of EscapeToBytes
-        /// </summary>
-        /// <remarks>
-        /// This variant uses a list of bytes.
-        /// </remarks>
-        /// <param name="data"></param>
-        /// <param name="flags"></param>
-        /// <param name="hexChars"></param>
-        /// <param name="allowedBytes"></param>
-        /// <returns></returns>
-        internal static byte[] EscapeToBytes1(byte[] data, UrlEscapeFlags flags, byte[] hexChars, ISet<byte> allowedBytes)
-        {
-            var encodedByte = new byte[3];
-            encodedByte[0] = (byte)'%';
-
-            var output = new List<byte>(data.Length);
-            var len = data.Length;
-            var escapeByte = false;
-            for (var i = 0; i != len; ++i)
-            {
-                var v = data[i];
-                if (allowedBytes.Contains(v))
-                {
-                    output.Add(v);
-                }
-                else if (v == 0x20)
-                {
-                    if ((flags & UrlEscapeFlags.EscapeSpaceAsPlus) != UrlEscapeFlags.Default)
-                    {
-                        output.Add((byte)'+');
-                    }
-                    else
-                    {
-                        escapeByte = true;
-                    }
-                }
-                else
-                {
-                    escapeByte = true;
-                }
-                if (escapeByte)
-                {
-                    escapeByte = false;
-                    var hiByte = ((v >> 4) & 0x0F);
-                    var loByte = (v & 0x0F);
-                    encodedByte[1] = hexChars[hiByte];
-                    encodedByte[2] = hexChars[loByte];
-                    output.AddRange(encodedByte);
-                }
-            }
-            return output.ToArray();
-        }
-
-        /// <summary>
-        /// Variant 2 of EscapeToBytes
-        /// </summary>
-        /// <remarks>
-        /// This variant uses a list of byte arrays
-        /// </remarks>
-        /// <param name="data"></param>
-        /// <param name="flags"></param>
-        /// <param name="hexChars"></param>
-        /// <param name="allowedBytes"></param>
-        /// <returns></returns>
-        internal static byte[] EscapeToBytes2(byte[] data, UrlEscapeFlags flags, byte[] hexChars, ISet<byte> allowedBytes)
-        {
-            var output = new List<byte[]>();
-            var outputEscaped = new List<byte>();
-            var outputSize = 0;
-            var len = data.Length;
-            var afterLastValid = 0;
-            var escapeByte = false;
-            for (var i = 0; i != len; ++i)
-            {
-                var v = data[i];
-                if (allowedBytes.Contains(v))
-                    continue;
-
-                var validBytes = i - afterLastValid;
-                if (validBytes != 0)
-                {
-                    if (outputEscaped.Count != 0)
-                    {
-                        var tempEscaped = outputEscaped.ToArray();
-                        outputSize += tempEscaped.Length;
-                        output.Add(tempEscaped);
-                        outputEscaped = new List<byte>();
-                    }
-
-                    var temp = new byte[validBytes];
-                    Array.Copy(data, afterLastValid, temp, 0, validBytes);
-                    output.Add(temp);
-                    outputSize += validBytes;
-                }
-                if (v == 0x20)
-                {
-                    if ((flags & UrlEscapeFlags.EscapeSpaceAsPlus) != UrlEscapeFlags.Default)
-                    {
-                        outputEscaped.Add((byte)'+');
-                    }
-                    else
-                    {
-                        escapeByte = true;
-                    }
-                }
-                else
-                {
-                    escapeByte = true;
-                }
-                if (escapeByte)
-                {
-                    escapeByte = false;
-                    var hiByte = ((v >> 4) & 0x0F);
-                    var loByte = (v & 0x0F);
-                    outputEscaped.Add((byte)'%');
-                    outputEscaped.Add(hexChars[hiByte]);
-                    outputEscaped.Add(hexChars[loByte]);
-                }
-                afterLastValid = i + 1;
-            }
-
-            if (outputEscaped.Count != 0)
-            {
-                var tempEscaped = outputEscaped.ToArray();
-                outputSize += tempEscaped.Length;
-                output.Add(tempEscaped);
-            }
-            {
-                var validBytes = len - afterLastValid;
-                if (validBytes != 0)
-                {
-                    var temp = new byte[validBytes];
-                    Array.Copy(data, afterLastValid, temp, 0, validBytes);
-                    output.Add(temp);
-                    outputSize += validBytes;
-                }
-            }
-
-            var result = new byte[outputSize];
-            var destOffset = 0;
-            foreach (var part in output)
-            {
-                var length = part.Length;
-                Array.Copy(part, 0, result, destOffset, length);
-                destOffset += length;
-            }
-
-            return result;
+            return s_defaultEscapeUtility.EscapeToBytes(data, flags);
         }
     }
 }
