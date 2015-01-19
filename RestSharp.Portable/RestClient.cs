FubarDevelopment/restsharp.portable

commit c856d2a40774ff197fedb4837726e5ffff696104

IRestClient now derives from IDisposable and keeps the HttpClient instance, which should fix #11

RestSharp.Portable/RestClient.cs
@@ -23,6 +23,7 @@ namespace RestSharp.Portable
         private readonly IList<string> _acceptEncodings = new List<string>();
         
         private readonly List<Parameter> _defaultParameters = new List<Parameter>();
+        private HttpClient _httpClient;
 
         /// <summary>
         /// HTTP client factory used to create IHttpClient implementations
@@ -129,6 +130,34 @@ namespace RestSharp.Portable
             }
         }
 
+        /// <summary>
+        /// Notify the authenticator about a failed request to be able to retry the request
+        /// with updated authentication information.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="response"></param>
+        /// <returns>true == Authenticator notified</returns>
+        private async Task<bool> NotifyAuthenticatorAboutFailedRequest(IRestRequest request, HttpResponseMessage response)
+        {
+            var asyncRoundTripAuthenticator = Authenticator as IAsyncRoundTripAuthenticator;
+            if (asyncRoundTripAuthenticator != null && asyncRoundTripAuthenticator.StatusCodes.Contains(response.StatusCode))
+            {
+                var restResponse = new RestResponse(this, request);
+                await restResponse.LoadResponse(response);
+                await asyncRoundTripAuthenticator.AuthenticationFailed(this, request, restResponse);
+                return true;
+            }
+            var roundTripAuthenticator = Authenticator as IRoundTripAuthenticator;
+            if (roundTripAuthenticator != null && roundTripAuthenticator.StatusCodes.Contains(response.StatusCode))
+            {
+                var restResponse = new RestResponse(this, request);
+                await restResponse.LoadResponse(response);
+                roundTripAuthenticator.AuthenticationFailed(this, request, restResponse);
+                return true;
+            }
+            return false;
+        }
+
         private async Task<HttpResponseMessage> ExecuteRequest(IRestRequest request, CancellationToken ct)
         {
             var retryWithAuthentication = true;
@@ -136,58 +165,42 @@ namespace RestSharp.Portable
             for (; ; )
             {
                 await AuthenticateRequest(request);
-                using (var httpClient = HttpClientFactory.CreateClient(this, request))
+                if (_httpClient == null)
+                    _httpClient = HttpClientFactory.CreateClient(this, request);
+                using (var message = HttpClientFactory.CreateRequestMessage(this, request))
                 {
-                    using (var message = HttpClientFactory.CreateRequestMessage(this, request))
+
+                    var bodyData = this.GetContent(request);
+                    if (bodyData != null)
+                        message.Content = bodyData;
+
+                    if (EnvironmentUtilities.IsSilverlight && message.Method == HttpMethod.Get)
+                        message.Headers.Accept.Clear();
+
+                    bool failed = true;
+                    var response = await _httpClient.SendAsync(message, ct);
+                    try
                     {
-
-                        var bodyData = this.GetContent(request);
-                        if (bodyData != null)
-                            message.Content = bodyData;
-
-                        if (EnvironmentUtilities.IsSilverlight && message.Method == HttpMethod.Get)
-                            message.Headers.Accept.Clear();
-
-                        bool failed = true;
-                        var response = await httpClient.SendAsync(message, ct);
-                        try
+                        if (retryWithAuthentication)
                         {
-                            if (retryWithAuthentication)
+                            retryWithAuthentication = false;
+                            var retry = await NotifyAuthenticatorAboutFailedRequest(request, response);
+                            if (retry)
                             {
-                                retryWithAuthentication = false;
-                                var asyncRoundTripAuthenticator = Authenticator as IAsyncRoundTripAuthenticator;
-                                if (asyncRoundTripAuthenticator != null && asyncRoundTripAuthenticator.StatusCodes.Contains(response.StatusCode))
-                                {
-                                    var restResponse = new RestResponse(this, request);
-                                    await restResponse.LoadResponse(response);
-                                    await asyncRoundTripAuthenticator.AuthenticationFailed(this, request, restResponse);
-                                    failed = false;
-                                    continue;
-                                }
-                                else
-                                {
-                                    var roundTripAuthenticator = Authenticator as IRoundTripAuthenticator;
-                                    if (roundTripAuthenticator != null && roundTripAuthenticator.StatusCodes.Contains(response.StatusCode))
-                                    {
-                                        var restResponse = new RestResponse(this, request);
-                                        await restResponse.LoadResponse(response);
-                                        roundTripAuthenticator.AuthenticationFailed(this, request, restResponse);
-                                        failed = false;
-                                        continue;
-                                    }
-                                }
+                                failed = false;
+                                continue;
                             }
-                            if (!IgnoreResponseStatusCode)
-                                response.EnsureSuccessStatusCode();
-                            failed = false;
                         }
-                        finally
-                        {
-                            if (failed && response != null)
-                                response.Dispose();
-                        }
-                        return response;
+                        if (!IgnoreResponseStatusCode)
+                            response.EnsureSuccessStatusCode();
+                        failed = false;
                     }
+                    finally
+                    {
+                        if (failed && response != null)
+                            response.Dispose();
+                    }
+                    return response;
                 }
             }
         }
@@ -448,5 +461,16 @@ namespace RestSharp.Portable
         /// If this property is null, the <see cref="StringComparer.Ordinal"/> is used.
         /// </remarks>
         public StringComparer DefaultParameterNameComparer { get; set; }
+
+        /// <summary>
+        /// Close the used HTTP client
+        /// </summary>
+        public void Dispose()
+        {
+            if (_httpClient == null)
+                return;
+            _httpClient.Dispose();
+            _httpClient = null;
+        }
     }
 }

RestSharp.Portable.Test/AuthenticationTests.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using System.Net.Http;
@@ -10,6 +9,8 @@ namespace RestSharp.Portable.Test
     [TestClass]
     public class AuthenticationTests
     {
+        [SuppressMessage("ReSharper", "ClassNeverInstantiated.Local")]
+        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
         class AuthenticationResult
         {
             public bool Authenticated { get; set; }
@@ -22,16 +23,20 @@ namespace RestSharp.Portable.Test
             const string username = "user name";
             const string password = "passwd";
 
-            var client = new RestClient("http://httpbin.org");
-            client.CookieContainer = new System.Net.CookieContainer();
-            client.Authenticator = new Authenticators.OptionalHttpBasicAuthenticator(username, password);
-            var request = new RestRequest("basic-auth/{username}/{password}", System.Net.Http.HttpMethod.Get);
-            request.AddUrlSegment("username", username);
-            request.AddUrlSegment("password", password);
-            var response = await client.Execute<AuthenticationResult>(request);
+            using (var client = new RestClient("http://httpbin.org")
+            {
+                CookieContainer = new System.Net.CookieContainer(),
+                Authenticator = new Authenticators.OptionalHttpBasicAuthenticator(username, password),
+            })
+            {
+                var request = new RestRequest("basic-auth/{username}/{password}", HttpMethod.Get);
+                request.AddUrlSegment("username", username);
+                request.AddUrlSegment("password", password);
+                var response = await client.Execute<AuthenticationResult>(request);
 
-            Assert.IsTrue(response.Data.Authenticated);
-            Assert.AreEqual(username, response.Data.User);
+                Assert.IsTrue(response.Data.Authenticated);
+                Assert.AreEqual(username, response.Data.User);
+            }
         }
 
         [TestMethod]
@@ -40,16 +45,20 @@ namespace RestSharp.Portable.Test
             const string username = "user name";
             const string password = "passwd";
 
-            var client = new RestClient("http://httpbin.org");
-            client.CookieContainer = new System.Net.CookieContainer();
-            client.Authenticator = new Authenticators.OptionalHttpBasicAuthenticator(username, password);
-            var request = new RestRequest("hidden-basic-auth/{username}/{password}", System.Net.Http.HttpMethod.Get);
-            request.AddUrlSegment("username", username);
-            request.AddUrlSegment("password", password);
-            var response = await client.Execute<AuthenticationResult>(request);
+            using (var client = new RestClient("http://httpbin.org")
+            {
+                CookieContainer = new System.Net.CookieContainer(),
+                Authenticator = new Authenticators.OptionalHttpBasicAuthenticator(username, password),
+            })
+            {
+                var request = new RestRequest("hidden-basic-auth/{username}/{password}", HttpMethod.Get);
+                request.AddUrlSegment("username", username);
+                request.AddUrlSegment("password", password);
+                var response = await client.Execute<AuthenticationResult>(request);
 
-            Assert.IsTrue(response.Data.Authenticated);
-            Assert.AreEqual(username, response.Data.User);
+                Assert.IsTrue(response.Data.Authenticated);
+                Assert.AreEqual(username, response.Data.User);
+            }
         }
 
         [TestMethod]
@@ -58,16 +67,20 @@ namespace RestSharp.Portable.Test
             const string username = "user name";
             const string password = "passwd";
 
-            var client = new RestClient("http://httpbin.org");
-            client.CookieContainer = new System.Net.CookieContainer();
-            client.Authenticator = new Authenticators.HttpDigestAuthenticator(new System.Net.NetworkCredential(username, password));
-            var request = new RestRequest("digest-auth/auth/{username}/{password}", System.Net.Http.HttpMethod.Get);
-            request.AddUrlSegment("username", username);
-            request.AddUrlSegment("password", password);
-            var response = await client.Execute<AuthenticationResult>(request);
+            using (var client = new RestClient("http://httpbin.org")
+            {
+                CookieContainer = new System.Net.CookieContainer(),
+                Authenticator = new Authenticators.HttpDigestAuthenticator(new System.Net.NetworkCredential(username, password))
+            })
+            {
+                var request = new RestRequest("digest-auth/auth/{username}/{password}", HttpMethod.Get);
+                request.AddUrlSegment("username", username);
+                request.AddUrlSegment("password", password);
+                var response = await client.Execute<AuthenticationResult>(request);
 
-            Assert.IsTrue(response.Data.Authenticated);
-            Assert.AreEqual(username, response.Data.User);
+                Assert.IsTrue(response.Data.Authenticated);
+                Assert.AreEqual(username, response.Data.User);
+            }
         }
 
         [TestMethod]
@@ -76,16 +89,20 @@ namespace RestSharp.Portable.Test
             const string username = "user name";
             const string password = "passwd";
 
-            var client = new RestClient("http://httpbin.org/digest-auth");
-            client.CookieContainer = new System.Net.CookieContainer();
-            client.Authenticator = new Authenticators.HttpDigestAuthenticator(new System.Net.NetworkCredential(username, password));
-            var request = new RestRequest("auth-int/{username}/{password}", System.Net.Http.HttpMethod.Get);
-            request.AddUrlSegment("username", username);
-            request.AddUrlSegment("password", password);
-            var response = await client.Execute<AuthenticationResult>(request);
+            using (var client = new RestClient("http://httpbin.org/digest-auth")
+            {
+                CookieContainer = new System.Net.CookieContainer(),
+                Authenticator = new Authenticators.HttpDigestAuthenticator(new System.Net.NetworkCredential(username, password))
+            })
+            {
+                var request = new RestRequest("auth-int/{username}/{password}", HttpMethod.Get);
+                request.AddUrlSegment("username", username);
+                request.AddUrlSegment("password", password);
+                var response = await client.Execute<AuthenticationResult>(request);
 
-            Assert.IsTrue(response.Data.Authenticated);
-            Assert.AreEqual(username, response.Data.User);
+                Assert.IsTrue(response.Data.Authenticated);
+                Assert.AreEqual(username, response.Data.User);
+            }
         }
 
         [TestMethod]
@@ -95,26 +112,29 @@ namespace RestSharp.Portable.Test
             var secret = System.Configuration.ConfigurationManager.AppSettings["bitbucket-api-secret"];
             var key = System.Configuration.ConfigurationManager.AppSettings["bitbucket-api-key"];
 
-            var client = new RestClient("https://bitbucket.org/api/");
-            client.CookieContainer = new System.Net.CookieContainer();
-            client.AddHandler("application/x-www-form-urlencoded", new DictionaryDeserializer());
-
-            var auth = Authenticators.OAuth1Authenticator.ForRequestToken(key, secret, "https://testapp.local/callback");
-            auth.ParameterHandling = Authenticators.OAuth.OAuthParameterHandling.UrlOrPostParameters;
-            client.Authenticator = auth;
-
-            string token, token_secret;
+            using (var client = new RestClient("https://bitbucket.org/api/")
             {
-                var request = new RestRequest("1.0/oauth/request_token", HttpMethod.Post);
-                var response = await client.Execute<IDictionary<string, string>>(request);
+                CookieContainer = new System.Net.CookieContainer()
+            })
+            {
+                client.AddHandler("application/x-www-form-urlencoded", new DictionaryDeserializer());
 
-                Assert.IsTrue(response.Data.ContainsKey("oauth_callback_confirmed"));
-                Assert.AreEqual("true", response.Data["oauth_callback_confirmed"]);
+                var auth = Authenticators.OAuth1Authenticator.ForRequestToken(key, secret, "https://testapp.local/callback");
+                auth.ParameterHandling = Authenticators.OAuth.OAuthParameterHandling.UrlOrPostParameters;
+                client.Authenticator = auth;
 
-                token_secret = response.Data["oauth_token_secret"];
-                token = response.Data["oauth_token"];
+                //string token, token_secret;
+                {
+                    var request = new RestRequest("1.0/oauth/request_token", HttpMethod.Post);
+                    var response = await client.Execute<IDictionary<string, string>>(request);
+
+                    Assert.IsTrue(response.Data.ContainsKey("oauth_callback_confirmed"));
+                    Assert.AreEqual("true", response.Data["oauth_callback_confirmed"]);
+
+                    //token_secret = response.Data["oauth_token_secret"];
+                    //token = response.Data["oauth_token"];
+                }
             }
-
         }
     }
 }

RestSharp.Portable.Test/RequestParameterTests.cs
@@ -11,109 +11,120 @@ namespace RestSharp.Portable.Test
         [TestMethod]
         public void TestParameterCaseSensitive()
         {
-            var client = new RestClient("http://localhost");
-            client.AddDefaultParameter("param1", "value1");
+            using (var client = new RestClient("http://localhost"))
+            {
+                client.AddDefaultParameter("param1", "value1");
 
-            var request = new RestRequest();
-            request
-                .AddParameter("param2", "value2")
-                .AddParameter("Param1", "value1.1");
+                var request = new RestRequest();
+                request
+                    .AddParameter("param2", "value2")
+                    .AddParameter("Param1", "value1.1");
 
-            var uri = client.BuildUri(request).ToString();
-            Assert.AreEqual("http://localhost/?param1=value1&param2=value2&Param1=value1.1", uri);
+                var uri = client.BuildUri(request).ToString();
+                Assert.AreEqual("http://localhost/?param1=value1&param2=value2&Param1=value1.1", uri);
+            }
         }
 
         [TestMethod]
         public void TestOnlyDefaultParameter()
         {
-            var client = new RestClient("http://localhost");
-            client.AddDefaultParameter("param1", "value1");
+            using (var client = new RestClient("http://localhost"))
+            {
+                client.AddDefaultParameter("param1", "value1");
 
-            var request = new RestRequest();
+                var request = new RestRequest();
 
-            var uri = client.BuildUri(request).ToString();
-            Assert.AreEqual("http://localhost/?param1=value1", uri);
+                var uri = client.BuildUri(request).ToString();
+                Assert.AreEqual("http://localhost/?param1=value1", uri);
+            }
         }
 
         [TestMethod]
         public void TestParameterCaseInsensitive()
         {
-            var client = new RestClient("http://localhost")
+            using (var client = new RestClient("http://localhost")
             {
                 DefaultParameterNameComparer = StringComparer.OrdinalIgnoreCase,
-            };
-            client.AddDefaultParameter("param1", "value1");
+            })
+            {
+                client.AddDefaultParameter("param1", "value1");
 
-            var request = new RestRequest();
-            request
-                .AddParameter("param2", "value2")
-                .AddParameter("Param1", "value1.1");
+                var request = new RestRequest();
+                request
+                    .AddParameter("param2", "value2")
+                    .AddParameter("Param1", "value1.1");
 
-            var uri = client.BuildUri(request).ToString();
-            Assert.AreEqual("http://localhost/?Param1=value1.1&param2=value2", uri);
+                var uri = client.BuildUri(request).ToString();
+                Assert.AreEqual("http://localhost/?Param1=value1.1&param2=value2", uri);
+            }
         }
 
         [TestMethod]
         public void TestParameterOverride()
         {
-            var client = new RestClient("http://localhost");
-            client.AddDefaultParameter("param1", "value1");
+            using (var client = new RestClient("http://localhost"))
+            {
+                client.AddDefaultParameter("param1", "value1");
 
-            var request = new RestRequest();
-            request
-                .AddParameter("param2", "value2")
-                .AddParameter("param1", "value1.1");
+                var request = new RestRequest();
+                request
+                    .AddParameter("param2", "value2")
+                    .AddParameter("param1", "value1.1");
 
-            var uri = client.BuildUri(request).ToString();
-            Assert.AreEqual("http://localhost/?param1=value1.1&param2=value2", uri);
+                var uri = client.BuildUri(request).ToString();
+                Assert.AreEqual("http://localhost/?param1=value1.1&param2=value2", uri);
+            }
         }
 
         [TestMethod]
         public void TestParameterDuplication()
         {
-            var client = new RestClient("http://localhost");
+            using (var client = new RestClient("http://localhost"))
+            {
+                var request = new RestRequest();
+                request
+                    .AddParameter("param1", "value1")
+                    .AddParameter("param2", "value2")
+                    .AddParameter("param1", "value1.1");
 
-            var request = new RestRequest();
-            request
-                .AddParameter("param1", "value1")
-                .AddParameter("param2", "value2")
-                .AddParameter("param1", "value1.1");
-
-            var uri = client.BuildUri(request).ToString();
-            Assert.AreEqual("http://localhost/?param1=value1.1&param2=value2", uri);
+                var uri = client.BuildUri(request).ToString();
+                Assert.AreEqual("http://localhost/?param1=value1.1&param2=value2", uri);
+            }
         }
 
         [TestMethod]
         public void TestParameterDuplicationGetAndQueryString()
         {
-            var client = new RestClient("http://localhost");
+            using (var client = new RestClient("http://localhost"))
+            {
+                var request = new RestRequest();
+                request
+                    .AddParameter("param1", "value1", ParameterType.GetOrPost)
+                    .AddParameter("param2", "value2")
+                    .AddParameter("param1", "value1.1", ParameterType.QueryString);
 
-            var request = new RestRequest();
-            request
-                .AddParameter("param1", "value1", ParameterType.GetOrPost)
-                .AddParameter("param2", "value2")
-                .AddParameter("param1", "value1.1", ParameterType.QueryString);
-
-            var uri = client.BuildUri(request).ToString();
-            Assert.AreEqual("http://localhost/?param1=value1.1&param2=value2", uri);
+                var uri = client.BuildUri(request).ToString();
+                Assert.AreEqual("http://localhost/?param1=value1.1&param2=value2", uri);
+            }
         }
 
         [TestMethod]
         public async Task TestParameterDuplicationPostAndQueryString()
         {
-            var client = new RestClient("http://localhost");
+            using (var client = new RestClient("http://localhost"))
+            {
+                var request = new RestRequest(HttpMethod.Post);
+                request
+                    .AddParameter("param1", "value1", ParameterType.GetOrPost)
+                    .AddParameter("param2", "value2")
+                    .AddParameter("param1", "value1.1", ParameterType.QueryString);
 
-            var request = new RestRequest(HttpMethod.Post);
-            request
-                .AddParameter("param1", "value1", ParameterType.GetOrPost)
-                .AddParameter("param2", "value2")
-                .AddParameter("param1", "value1.1", ParameterType.QueryString);
+                var uri = client.BuildUri(request).ToString();
+                Assert.AreEqual("http://localhost/?param1=value1.1", uri);
 
-            var uri = client.BuildUri(request).ToString();
-            Assert.AreEqual("http://localhost/?param1=value1.1", uri);
-
-            var body = await client.GetContent(request).ReadAsStringAsync();
-            Assert.AreEqual("param1=value1&param2=value2", body);
+                var body = await client.GetContent(request).ReadAsStringAsync();
+                Assert.AreEqual("param1=value1&param2=value2", body);
+            }
         }
     }
 }

RestSharp.Portable/IRestClient.cs
@@ -12,7 +12,7 @@ namespace RestSharp.Portable
     /// <summary>
     /// REST client
     /// </summary>
-    public interface IRestClient
+    public interface IRestClient : IDisposable
     {
         /// <summary>
         /// Authenticator to use for all requests

commit aa13d48880b886153e1239093624e810235368f0

Fix SL5 compatibility patch broken in c856d2a

RestSharp.Portable/RestClient.cs
@@ -175,7 +175,7 @@ namespace RestSharp.Portable
                         message.Content = bodyData;
 
                     if (EnvironmentUtilities.IsSilverlight && message.Method == HttpMethod.Get)
-                        message.Headers.Accept.Clear();
+                        _httpClient.DefaultRequestHeaders.Accept.Clear();
 
                     bool failed = true;
                     var response = await _httpClient.SendAsync(message, ct);