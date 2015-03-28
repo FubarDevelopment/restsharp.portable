3055c93c701aeea3d2621087dcd929886a31b3ba
Mark Junker <mark.junker75@gmail.com>
2015-03-28 19:28:50 +0100

    Changed the authenticator for the new authentication manager. (+3 squashed commit)
    Squashed commit:
    
    [847e6a2] Changed the authenticator for the changed authentication manager
    
    [246cc45] Changed IRestResponse to HttpResponseMessage for the authentication modules
    
    [baddd4a] First version of the refactored authentication modules

--- RestSharp.Portable.OAuth2/OAuth2Authenticator.cs
+++ RestSharp.Portable.OAuth2/OAuth2Authenticator.cs
@@ -15,9 +15,8 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace RestSharp.Portable.Authenticators
@@ -27,25 +26,13 @@ namespace RestSharp.Portable.Authenticators
     /// </summary>
     /// <remarks>
     /// Since there are many ways to authenticate in OAuth2,
-    /// this is used as a base class to differentiate between 
+    /// this is used as a base class to differentiate between
     /// other authenticators.
-    /// 
     /// Any other OAuth2 authenticators must derive from this
     /// abstract class.
     /// </remarks>
-    public abstract class OAuth2Authenticator : AsyncAuthenticationModule, IAsyncRoundTripAuthenticator, IRoundTripAuthenticator
+    public abstract class OAuth2Authenticator : AsyncAuthenticationModule
     {
-        /// <summary>
-        /// The OAuth client that is used by this authenticator
-        /// </summary>
-        protected OAuth2.OAuth2Client Client { get; private set; }
-
-        private static readonly IEnumerable<HttpStatusCode> _statusCodes = new List<HttpStatusCode>
-        {
-            HttpStatusCode.Unauthorized,
-        };
-        private static readonly IEnumerable<HttpStatusCode> _noStatusCodes = new List<HttpStatusCode>();
-
         /// <summary>
         /// Initializes a new instance of the <see cref="OAuth2Authenticator"/> class.
         /// </summary>
@@ -56,143 +43,46 @@ namespace RestSharp.Portable.Authenticators
         }
 
         /// <summary>
-        /// Will be called when the authentication failed
+        /// The OAuth client that is used by this authenticator
+        /// </summary>
+        protected OAuth2.OAuth2Client Client { get; private set; }
+
+        /// <summary>
+        /// Gets the authentication type provided by this authentication module.
+        /// </summary>
+        public override string AuthenticationType
+        {
+            get { return "OAuth 2.0"; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the authentication module supports pre-authentication.
+        /// </summary>
+        public override bool CanPreAuthenticate
+        {
+            get { return true; }
+        }
+
+        /// <summary>
+        /// Will be called in response to an authentication request.
         /// </summary>
         /// <param name="client">Client executing this request</param>
         /// <param name="request">Request to authenticate</param>
         /// <param name="response">Response of the failed request</param>
-        /// <returns>Task where the handler for a failed authentication gets executed</returns>
-        public virtual async Task AuthenticationFailed(IRestClient client, IRestRequest request, IRestResponse response)
+        /// <param name="header">Authentication/Authorization header</param>
+        /// <param name="credential">Credential to be used for the authentication</param>
+        /// <returns>The task the authentication is performed on, which returns true when the authentication succeeded</returns>
+        public override async Task<bool> Authenticate(
+            IRestClient client,
+            IRestRequest request,
+            HttpResponseMessage response,
+            AuthHeader header,
+            NetworkCredential credential)
         {
             if (string.IsNullOrEmpty(Client.RefreshToken))
-                return;
+                return false;
             await Client.GetCurrentToken(forceUpdate: true);
-        }
-
-        /// <summary>
-        /// Returns all the status codes where a round trip is allowed
-        /// </summary>
-        public virtual IEnumerable<HttpStatusCode> StatusCodes
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(Client.RefreshToken))
-                    return _noStatusCodes;
-                return _statusCodes;
-            }
-        }
-
-        /// <summary>
-        /// Will be called when the authentication failed
-        /// </summary>
-        /// <param name="client">Client executing this request</param>
-        /// <param name="request">Request to authenticate</param>
-        /// <param name="response">Response of the failed request</param>
-        void IRoundTripAuthenticator.AuthenticationFailed(IRestClient client, IRestRequest request, IRestResponse response)
-        {
-            AuthenticationFailed(client, request, response).Wait();
-        }
-    }
-
-    /// <summary>
-    /// The OAuth 2 authenticator using URI query parameter.
-    /// </summary>
-    /// <remarks>
-    /// Based on http://tools.ietf.org/html/draft-ietf-oauth-v2-10#section-5.1.2
-    /// </remarks>
-    public class OAuth2UriQueryParameterAuthenticator : OAuth2Authenticator
-    {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OAuth2UriQueryParameterAuthenticator"/> class.
-        /// </summary>
-        /// <param name="client">The OAuth2 client</param>
-        public OAuth2UriQueryParameterAuthenticator(OAuth2.OAuth2Client client)
-            : base(client) { }
-
-        /// <summary>
-        /// Modifies the request to ensure that the authentication requirements are met.
-        /// </summary>
-        /// <param name="client">Client executing this request</param>
-        /// <param name="request">Request to authenticate</param>
-        /// <returns></returns>
-        public override async Task Authenticate(IRestClient client, IRestRequest request)
-        {
-            request.AddParameter("oauth_token", await Client.GetCurrentToken(), ParameterType.GetOrPost);
-        }
-    }
-
-    /// <summary>
-    /// The OAuth 2 authenticator using the authorization request header field.
-    /// </summary>
-    /// <remarks>
-    /// Based on http://tools.ietf.org/html/draft-ietf-oauth-v2-10#section-5.1.1
-    /// </remarks>
-    public class OAuth2AuthorizationRequestHeaderAuthenticator : OAuth2Authenticator
-    {
-        private readonly string _tokenType;
-        private bool _authFailed;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OAuth2AuthorizationRequestHeaderAuthenticator"/> class.
-        /// </summary>
-        /// <param name="client">The OAuth2 client</param>
-        public OAuth2AuthorizationRequestHeaderAuthenticator(OAuth2.OAuth2Client client)
-            : this(client, (string.IsNullOrEmpty(client.TokenType) ? "OAuth" : client.TokenType)) { }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="OAuth2AuthorizationRequestHeaderAuthenticator"/> class.
-        /// </summary>
-        /// <param name="client">The OAuth2 client</param>
-        /// <param name="tokenType">
-        /// The token type.
-        /// </param>
-        public OAuth2AuthorizationRequestHeaderAuthenticator(OAuth2.OAuth2Client client, string tokenType)
-            : base(client)
-        {
-            _tokenType = tokenType;
-        }
-
-        /// <summary>
-        /// Will be called when the authentication failed
-        /// </summary>
-        /// <param name="client">Client executing this request</param>
-        /// <param name="request">Request to authenticate</param>
-        /// <param name="response">Response of the failed request</param>
-        /// <returns>Task where the handler for a failed authentication gets executed</returns>
-        public override async Task AuthenticationFailed(IRestClient client, IRestRequest request, IRestResponse response)
-        {
-            if (string.IsNullOrEmpty(Client.RefreshToken))
-                return;
-            // Set this variable only if we have a refresh token
-            _authFailed = true;
-            await Client.GetCurrentToken(forceUpdate: true);
-        }
-
-        /// <summary>
-        /// Modifies the request to ensure that the authentication requirements are met.
-        /// </summary>
-        /// <param name="client">Client executing this request</param>
-        /// <param name="request">Request to authenticate</param>
-        /// <returns></returns>
-        public override async Task Authenticate(IRestClient client, IRestRequest request)
-        {
-            // Only add the Authorization parameter if it hasn't been added and the authorization didn't fail previously
-            var authParam = request.Parameters.LastOrDefault(p => p.Type == ParameterType.HttpHeader && p.Name.Equals("Authorization", StringComparison.OrdinalIgnoreCase));
-            if (!_authFailed && authParam != null)
-                return;
-
-            // When the authorization failed or when the Authorization header is missing, we're just adding it (again) with the
-            // new AccessToken.
-            _authFailed = false;
-            var authValue = string.Format("{0} {1}", _tokenType, await Client.GetCurrentToken());
-            if (authParam == null)
-            {
-                request.AddParameter("Authorization", authValue, ParameterType.HttpHeader);
-            }
-            else
-            {
-                authParam.Value = authValue;
-            }
+            return true;
         }
     }
 }
