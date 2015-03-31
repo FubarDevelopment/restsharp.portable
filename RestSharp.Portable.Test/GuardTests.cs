4cfb15653c76b35028988e3dcc5231d5cf94fe64
Mark Junker <mark.junker75@gmail.com>
2015-04-01 01:20:45 +0200

    Forgot to set the results in the guarded block

--- RestSharp.Portable.Test/GuardTests.cs
+++ RestSharp.Portable.Test/GuardTests.cs
@@ -12,8 +12,7 @@ namespace RestSharp.Portable.Test
         [Fact]
         public void TestGuardLock1()
         {
-            var evt1 = new AutoResetEvent(false);
-            var evt2 = new AutoResetEvent(false);
+            var evt = new AutoResetEvent(false);
             var guard = new RequestGuard();
             var results = new ConcurrentBag<int>();
             var t1 = Task.Run(
@@ -21,24 +20,20 @@ namespace RestSharp.Portable.Test
                 {
                     using (guard.Guard(CancellationToken.None))
                     {
-                        evt1.Set();
-                        evt2.WaitOne();
-                        evt1.WaitOne();
+                        evt.Set();
+                        evt.WaitOne();
                         Thread.Sleep(TimeSpan.FromMilliseconds(500));
+                        results.Add(1);
                     }
-
-                    results.Add(1);
                 });
-            evt1.WaitOne();
+            evt.WaitOne();
 
             var t2 = Task.Run(
                 () =>
                 {
-                    evt2.Set();
-                    evt1.Set();
+                    evt.Set();
                     using (guard.Guard(CancellationToken.None))
-                        DoNothing();
-                    results.Add(2);
+                        results.Add(2);
                 });
 
             Task.WaitAll(t1, t2);
@@ -57,24 +52,21 @@ namespace RestSharp.Portable.Test
                 {
                     Thread.Sleep(TimeSpan.FromMilliseconds(100));
                     using (guard.Guard(CancellationToken.None))
-                        DoNothing();
-                    results.Add(2);
+                        results.Add(2);
                 });
             var t1 = Task.Run(
                 () =>
                 {
                     using (guard.Guard(CancellationToken.None))
+                    {
                         Thread.Sleep(TimeSpan.FromMilliseconds(500));
-                    results.Add(1);
+                        results.Add(1);
+                    }
                 });
             Task.WaitAll(t1, t2);
             var resultsData = results.ToArray();
             Assert.Equal(1, resultsData[0]);
             Assert.Equal(2, resultsData[1]);
-        }
-
-        private static void DoNothing()
-        {
         }
     }
 }
