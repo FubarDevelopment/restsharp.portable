335dda5d0e5fdb5bb5947505380686d1860fa719
Mark Junker <m.junker@fubar-dev.de>
2015-04-07 10:54:36 +0200

    Fixed merge

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -24,11 +24,18 @@ using System.Runtime.CompilerServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern
 // durch Einsatz von '*', wie in nachfolgendem Beispiel:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.4.0")]
-[assembly: AssemblyFileVersion("2.4.1")]
-[assembly: AssemblyInformationalVersion("2.4.1")]
+[assembly: AssemblyVersion("3.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.1")]
+[assembly: AssemblyInformationalVersion("3.0.0.1")]
 
 [assembly: InternalsVisibleTo("RestSharp.Portable.OAuth, PublicKey=" +
+"00240000048000009400000006020000002400005253413100040000010001000f8415df6f1232" +
+"a8e57ec65d511e606ce7ecdd04e8dfeff5518e27ad449b9ccce658c1b0dc5e73e86626540b7a1e" +
+"c3c2f9e69f37db4626ad2207a4b4c5c3bf064d2a7c7766961b5f55d16d4082753d0c4be118e658" +
+"9bb2ee7a911ad43721beea1f034c3d8a87c97f5de3e575cc0db2db79d396e69eeef2461ff8615f" +
+"48df7bba")]
+
+[assembly: InternalsVisibleTo("RestSharp.Portable.OAuth1.Tests, PublicKey=" +
 "00240000048000009400000006020000002400005253413100040000010001000f8415df6f1232" +
 "a8e57ec65d511e606ce7ecdd04e8dfeff5518e27ad449b9ccce658c1b0dc5e73e86626540b7a1e" +
 "c3c2f9e69f37db4626ad2207a4b4c5c3bf064d2a7c7766961b5f55d16d4082753d0c4be118e658" +
