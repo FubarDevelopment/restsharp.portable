fd2cbf5d7be321d9f7eed9ed3e17551d028026d1
Mark Junker <fubar-coder@live.de>
2015-11-15 16:24:18 +0100

    Delete old *.nupkg files

--- nuget-pack.ps1
+++ nuget-pack.ps1
@@ -6,6 +6,8 @@ param(
 	$config = "Release"
 )
 
+remove-item *.nupkg
+
 & nuget pack .\RestSharp.Portable.nuspec -Properties "clientversion=$version" -Version $version
 & nuget pack .\RestSharp.Portable.Core.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
 & nuget pack .\RestSharp.Portable.HttpClient.nuspec -Properties "Configuration=$config;clientversion=$version" -Version $version
