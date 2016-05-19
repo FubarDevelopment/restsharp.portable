7dbacf0c1aae2f6e9e8b5ccbd93dbd3dbbbc0412
Mark Junker <fubar-coder@live.de>
2016-05-20 00:59:59 +0200

    Utility script, because dotnet pack doesn't support globbing yet

---
+++ pack.cmd
@@ -0,0 +1,8 @@
+@echo off
+set BUILD_CFG=%1
+if "%BUILD_CFG%" equ "" set BUILD_CFG=Debug
+if exist "package-output" del "package-output" /f /q
+for /d %%D in (src\FubarCoder.RestSharp.Portable*) do (
+	dotnet pack %%D -c %BUILD_CFG% -o "package-output" --no-build --version-suffix "%2"
+	if %errorlevel% neq  0 exit /b %errorlevel%
+)
