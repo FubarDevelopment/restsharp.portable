cc6e12c13ad954621628708dc31fd418be25d939
Mark Junker <m.junker@fubar-dev.de>
2016-09-19 12:05:29 +0200

    No version suffix any more

--- pack.cmd
+++ pack.cmd
@@ -3,6 +3,6 @@ set BUILD_CFG=%1
 if "%BUILD_CFG%" equ "" set BUILD_CFG=Debug
 if exist "package-output" del "package-output" /f /q
 for /d %%D in (src\FubarCoder.RestSharp.Portable*) do (
-	dotnet pack %%D -c %BUILD_CFG% -o "package-output" --no-build --version-suffix "%2"
+	dotnet pack %%D -c %BUILD_CFG% -o "package-output" --no-build
 	if %errorlevel% neq  0 exit /b %errorlevel%
 )
